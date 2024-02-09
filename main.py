d939b05d39bd186b8fffa830aa75217fc5ebe013
Adam Shire <shirea@mit.edu>
2024-02-09 14:49:26 -0500

    adds a modal to create a new channel

--- main.py
+++ main.py
@@ -11,51 +11,53 @@ from puzbot.puzzle import Puzzle
 logging.basicConfig(level=logging.INFO)
 
 # process_before_response must be True when running on FaaS
-app = App(process_before_response=True, signing_secret=os.getenv("SIGNING_SECRET"), token=os.getenv("BOT_TOKEN"))
+app = App(
+    process_before_response=True,
+    signing_secret=os.getenv("SIGNING_SECRET"),
+    token=os.getenv("BOT_TOKEN"),
+)
+
 
 # Put some basic information on the bot home tab.
 @app.event("app_home_opened")
 def update_home_tab(client, event, logger):
-  try:
-    client.views_publish(
-      user_id = event["user"],
-      view={
-        "type": "home",
-        "callback_id": "home_view",
-        "blocks": [
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "*Welcome to PuzBot!* :tada:"
-            }
-          },
-          {
-            "type": "divider"
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "PuzBot is our attempt to streamline the mechanics of starting and stopping activity on a puzzle. More information about this is coming soon."
-            }
-          },
-          {
-            "type": "divider"
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "To contribute to Puzbot, please visit <https://github.com/adamshire123/puz-bot|*the Github repo*>"
-            }
-          }
-        ]
-      }
-    )
+    try:
+        client.views_publish(
+            user_id=event["user"],
+            view={
+                "type": "home",
+                "callback_id": "home_view",
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "*Welcome to PuzBot!* :tada:",
+                        },
+                    },
+                    {"type": "divider"},
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "PuzBot is our attempt to streamline the mechanics of starting and stopping activity on a puzzle. More information about this is coming soon.",
+                        },
+                    },
+                    {"type": "divider"},
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "To contribute to Puzbot, please visit <https://github.com/adamshire123/puz-bot|*the Github repo*>",
+                        },
+                    },
+                ],
+            },
+        )
 
-  except Exception as e:
-    logger.error(f"Error publishing home tab: {e}")
+    except Exception as e:
+        logger.error(f"Error publishing home tab: {e}")
+
 
 # for testing
 @app.command("/hello")
@@ -64,6 +66,7 @@ def hello_command(body, say, ack):
     ack()
     say("Hi from Google Cloud Functions!")
 
+
 # for testing
 @app.event("app_mention")
 def event_test(body, say, ack):
@@ -71,52 +74,96 @@ def event_test(body, say, ack):
     ack()
     say("Hi from Google Cloud Functions!")
 
+
 # kick off new puzzle creation
 @app.command("/init")
 def initialize_puzzle(body, say, ack):
-    ack('initializing puzzle...')
-    if body['text']:
+    ack("initializing puzzle...")
+    if body["text"]:
         send_pub_sub(body)
     else:
         say("please provide a puzzle name: e.g. puz-salad-daze")
 
+
 # Listener for the demonstration modal
 @app.shortcut("demonstrate_modal")
 def demonstrate_modal(ack, shortcut, client, logger):
-  ack()
-  client.views_open(
-    trigger_id=shortcut["trigger_id"],
-    view={
-      "type": "modal",
-      "title": {"type": "plain_text", "text": "Demonstration modal box"},
-      "close": {"type": "plain_text", "text": "Close"},
-      "blocks": [
-        {
-          "type": "section",
-          "text": {
-            "type": "mrkdwn",
-            "text": "This is a demonstration of a modal box invoked by a shortcut. If this were a working form, there would be input boxes and a submit button.\n\nThis is only a demonstration."
-          }
-        }
-      ]
-    }
-  )
+    ack()
+    client.views_open(
+        trigger_id=shortcut["trigger_id"],
+        view={
+            "type": "modal",
+            "callback_id": "new_puzzle",
+            "title": {
+                "type": "plain_text",
+                "text": "Set up a new puzzle",
+                "emoji": True,
+            },
+            "submit": {"type": "plain_text", "text": "Submit", "emoji": True},
+            "close": {"type": "plain_text", "text": "Cancel", "emoji": True},
+            "blocks": [
+                {
+                    "type": "input",
+                    "block_id": "puzzle_name",
+                    "element": {
+                        "type": "plain_text_input",
+                        "max_length": 80,
+                        "action_id": "puzzle_name_input-action",
+                    },
+                    "label": {
+                        "type": "plain_text",
+                        "text": "puzzle name",
+                        "emoji": True,
+                    },
+                },
+                {
+                    "type": "input",
+                    "block_id": "puzzle_url",
+                    "element": {
+                        "type": "url_text_input",
+                        "action_id": "puzzle_url_input-action",
+                    },
+                    "label": {
+                        "type": "plain_text",
+                        "text": "puzzle URL",
+                        "emoji": True,
+                    },
+                },
+            ],
+        },
+    )
+
+
 @app.command("/create-puzzle")
 def create_puzzle(ack, client, logger, say, command):
-  ack()
-  say(f"I have been requested to create a puzzle: \"{command['text']}\".")
-  # Create the Puzzle object, through which the Slack channel and Google sheet
-  # will be created.
-  draft = Puzzle()
-  draft.create(client, logger, say, command['text'])
-  say(f"I have finished attempting to create this puzzle.")
+    ack()
+    say(f"I have been requested to create a puzzle: \"{command['text']}\".")
+    # Create the Puzzle object, through which the Slack channel and Google sheet
+    # will be created.
+    draft = Puzzle()
+    draft.create(client, logger, say, command["text"])
+    say("I have finished attempting to create this puzzle.")
+
+
+@app.view("new_puzzle")
+def handle_submission(ack, body, client, view, logger, say):
+    ack()
+    puzzle_name = view["state"]["values"]["puzzle_name"]["puzzle_name_input-action"][
+        "value"
+    ]
+    logger.info(puzzle_name)
+    draft = Puzzle()
+    draft.create(client, logger, say, puzzle_name)
+
 
 handler = SlackRequestHandler(app)
 
+
 # Cloud Function Entry Point
 def cloud_function_handler(req):
-   
+
     return handler.handle(req)
+
 
 def send_pub_sub(data):
 
@@ -124,10 +171,10 @@ def send_pub_sub(data):
     topic_id = os.getenv("PUBSUB_TOPIC")
 
     publisher = pubsub_v1.PublisherClient()
-   
+
     topic_path = publisher.topic_path(project_id, topic_id)
 
     future = publisher.publish(topic_path, data=json.dumps(data).encode("utf8"))
-    
+
     logging.info(future.result())
     logging.info(f"Published message to {topic_path}.")
--- puzbot/puzzle.py
+++ puzbot/puzzle.py
@@ -2,40 +2,46 @@
 from __future__ import absolute_import
 import os, logging, re
 
-class Puzzle():
-  """
-  This is a wrapper for a puzzle. The intent is to use this to handle both the
-  management of the Slack channel, as well as the Google Sheet, for a puzzle.
-  This includes creation and solution events.
-  """
 
-  def __init__(self):
-    self.data = {}
-    self.data["channel-name"] = ""
-    self.data["name"] = ""
+class Puzzle:
+    """
+    This is a wrapper for a puzzle. The intent is to use this to handle both the
+    management of the Slack channel, as well as the Google Sheet, for a puzzle.
+    This includes creation and solution events.
+    """
 
-  def create(self, client, logger, say, puzzle_name):
-    self.data["name"] = puzzle_name
-    self.data["channel-name"] = self.set_channel_name(logger, puzzle_name)
+    def __init__(self):
+        self.data = {}
+        self.data["channel-name"] = ""
+        self.data["name"] = ""
 
-    try:
-      # Call the conversations.create method using the WebClient
-      # conversations_create requires the channels:manage bot scope
-      result = client.conversations_create(
-        name=self.data["channel-name"]
-      )
-      # Log the result which includes information like the ID of the conversation
-      logger.info(result)
-      say(f"The channel #{self.data['channel-name']} has been created!")
+    def create(self, client, logger, say, puzzle_name):
+        self.data["name"] = puzzle_name
+        self.data["channel-name"] = self.set_channel_name(logger, puzzle_name)
 
-    except Exception as e:
-      # There is a more specific error which could be caught, but this works.
-      logger.error("Error creating channel: {}".format(e))
-      say(f"I tried to create the channel #{self.data['channel-name']}. Unfortunately, I failed.")
+        try:
+            # Call the conversations.create method using the WebClient
+            # conversations_create requires the channels:manage bot scope
+            result = client.conversations_create(name=self.data["channel-name"])
+            # Log the result which includes information like the ID of the conversation
+            logger.info(result)
+            client.chat_postMessage(
+                text=f"The channel #{self.data['channel-name']} has been created!",
+                channel="#general",
+            )
 
-  def set_channel_name(self, logger, puzzle_name):
-    # strip leading or trailing whitespace
-    # convert to lower case
-    # replace any number of spaces between words with one dash
-    # prefix with "puzzle"
-    return "puzzle-" + re.sub(pattern=r" +", repl="-", string=puzzle_name.strip().lower())
+        except Exception as e:
+            # There is a more specific error which could be caught, but this works.
+            logger.error("Error creating channel: {}".format(e))
+            say(
+                f"I tried to create the channel #{self.data['channel-name']}. Unfortunately, I failed."
+            )
+
+    def set_channel_name(self, logger, puzzle_name):
+        # strip leading or trailing whitespace
+        # convert to lower case
+        # replace any number of spaces between words with one dash
+        # prefix with "puzzle"
+        return "puzzle-" + re.sub(
+            pattern=r" +", repl="-", string=puzzle_name.strip().lower()
+        )
